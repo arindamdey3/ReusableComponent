ebc4aa33bad60e83a47dc963b6f1311990d17391
Arindam Dey <arindam.dey3@cognizant.com>
2016-08-08 13:51:10 +0530

    Initial Commit

---
+++ ReusableComponent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReusableComponent
+//
+//  Created by KoliMac1 on 08/08/16.
+//  Copyright © 2016 KoliMac1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
